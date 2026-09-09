09f284a13b8b00b499b0e5ce8ec548afd7eb760d
shivammmmmmm <shivamsrawat7@gmail.com>
2026-09-09 19:59:05 +0530

    new code

--- apps/web/app/components/TreeStructure/TreeStructure.tsx
+++ apps/web/app/components/TreeStructure/TreeStructure.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useParams } from "next/navigation";
 import { useTreeStructureStore } from "../../store/TreeStructureStore";
-import { Tree } from "../tree/Tree";
+import { Tree } from "../Tree/tree";
 import { useFileContextMenuStore } from "@/store/fileContextMenuStore";
 import { FileContextMenu } from "../ContextMenu/FileContentMenu";
 
